dd048841dba1c83dcc1e1f84d775ddfe2823c345
josh co <joshcomstock9777@gmail.com>
2026-09-02 08:36:57 -0400

    Use type-only editor imports in Node contract path

--- src/modules/editor/companion.ts
+++ src/modules/editor/companion.ts
@@ -1,4 +1,4 @@
-import { EditorCommand, EditorAdapter } from '../../types.ts';
+import type { EditorCommand, EditorAdapter } from '../../types.ts';
 
 export type CompanionMode = 'suggest' | 'execute';
 
